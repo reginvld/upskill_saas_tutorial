9a5a38be3e96101213c93e0b4075082cb9a6ed49
Reggie Cooper <reginvld@gmail.com>
2016-12-13 04:01:20 +0000

    Cleaned up contact form routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,7 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+  
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
@@ -12,6 +13,7 @@ class ContactsController < ApplicationController
       redirect_to new_contact_path
     end
   end
+  
   private
     def contact_params
        params.require(:contact).permit(:name, :email, :comments)
